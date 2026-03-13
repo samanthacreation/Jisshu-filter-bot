648fabade53903d2a6011b568dfb1faecdde2caa
samanthacreation <samanthacreation648@gmail.com>
2026-03-13 20:44:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -34,7 +34,7 @@ USERNAME = environ.get("USERNAME", "https://t.me/owner_of_sk")  # ADMIN USERNAME
 # Database Channel
 CHANNELS = [
     int(ch) if id_pattern.search(ch) else ch
-    for ch in environ.get("CHANNELS", "-1002889685652").split()
+    for ch in environ.get("CHANNELS", "-1003544821271").split()
 ]
 
 # ForceSub Channel & Log Channels
